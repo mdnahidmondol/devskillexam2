d0c671c812685e525700d5dc1c115194661b707c
Md Nahid Mondol <nahidmondol444@gmail.com>
2021-05-31 16:03:50 +0000

    git init

---
+++ QuestionOne/QuestionOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QuestionOne
+{
+    class Program
+    {
+        public static void Print(string text1, string textt2)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
+        }
+        public static void Print(string text1, string text2, string text3)
+        {
+            Console.WriteLine(text1);
+            Console.WriteLine(text2);
+            Console.WriteLine(text3);
+        }
+    }
+}
---
+++ QuestionOne/questiontwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace questiontwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
